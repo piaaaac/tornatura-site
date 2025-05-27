be8ea3db0f730faf0d83a53ce85bae4683c9f05b
Alex Piacentini <piacentini.alex@gmail.com>
2025-05-27 16:27:40 +0200

    ga added

---
+++ site/snippets/ga.php
@@ -0,0 +1,18 @@
+<?php
+
+/** 
+ * Via Eleonora Ocello, mail 27.05.2025, 16:14  
+ */
+?>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EKZLEWFGS7"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-EKZLEWFGS7');
+</script>
